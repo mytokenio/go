4e3a951b4ab71202b51b651d7d18c70affecd591
twinsant <twinsant@gmail.com>
2019-06-27 20:55:59 +0800

    Add: test for aliyun sls hook.

---
+++ log/test/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"../../log"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.WithField("kk", "vv", "xxx").Infof("test debug log %s", "ddd")
+	log.Infof("test info log %s", "ddd")
+	log.Warnf("test warn log %s", "ddd")
+
+	log.RefreshUniqueId()
+
+	log.Errorf("test error log %s", "ddd")
+	log.WithFields(logrus.Fields{"aa": "bb", "cc": logrus.Fields{"dd": "aaaaaaa"}}, "test").Infof("test obj")
+
+	log.Type("test").Info("fdsafdas")
+
+	fields := logrus.Fields{
+		"aa": "bb",
+		"cc": "dd",
+	}
+	log.Type("test").WithFields(fields).Info("log with fields and type")
+
+	ch := make(chan os.Signal)
+	signal.Notify(ch)
+	if _, ok := <-ch; ok {
+		fmt.Println("Get the shutdown signal and start to shut down")
+	}
+}
